8b43999c3b8cc5deb3a836bf89d0f95c41405ddc
Tech-A67 <mnash@spartaglobal.com>
2018-05-20 16:20:59 +0100

    added db tables for user/chapter/project, set relationships within models

---
+++ COYI/app/models/project.rb
@@ -0,0 +1,4 @@
+class Project < ApplicationRecord
+  belongs_to :user
+  has_one :chapter, through :user
+end
--- COYI/config/routes.rb
+++ COYI/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :chapters
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+  
 end
