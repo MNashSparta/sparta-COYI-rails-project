fe1ab546e02c14e67038329652267e0755cddf2c
Tech-A64 <tech-a64@Tech-A64s-MacBook-Air.local>
2018-05-21 20:33:05 +0100

    Change password complexity validation from a gem to regular expression and increase password length requirement to 8 characters

--- COYI/app/models/user.rb
+++ COYI/app/models/user.rb
@@ -1,8 +1,44 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :chapter
+  has_many :chapters
   has_many :projects
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
+         :validatable, authentication_keys: [:login]
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+    where(conditions.to_h).first
+    end
+  end
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  validate :password_complexity
+
+  def password_complexity
+   if password.present?
+      if !password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)./)
+        errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one number"
+      end
+   end
+  end
 end
--- COYI/config/routes.rb
+++ COYI/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "chapters#index"
+  root to: "projects#index"
 
 end
