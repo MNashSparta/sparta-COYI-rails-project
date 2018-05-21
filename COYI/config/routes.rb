600e43a119f119d50f7f4834e21749149a7cd5ae
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-05-21 15:26:12 +0100

    about pages done

--- COYI/config/routes.rb
+++ COYI/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'about/about'
+  get 'about/contact'
+  get 'about/faq'
   resources :projects
   resources :chapters
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "chapters#index"
+
+  get 'about', to: 'about#about'
 
 end
