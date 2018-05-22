f90370703e3ecc90a2ecc42fd85f5a59a3033d93
Sykander <sgul@spartaglobal.com>
2018-05-22 14:06:34 +0100

    fixes with sophie

--- COYI/config/routes.rb
+++ COYI/config/routes.rb
@@ -1,28 +1,18 @@
 Rails.application.routes.draw do
 
+  root to: 'home#landing'
+
   get 'circle/global', to: 'circle#global', as: 'global'
   get 'circle/chapter', to: 'circle#chapter', as: 'chapter'
   get 'circle/advocate', to: 'circle#advocate', as: 'advocate'
-  get 'news', to: 'news#index', as: 'news'
   get 'circle/rules', to: 'circle#rules', as: 'rules'
-  get '/stories',  to: 'sucess#stories', as: 'stories'
-  root to: 'home#landing'
+  get 'news', to: 'news#index', as: 'news'
+  get 'stories',  to: 'sucess#stories', as: 'stories'
   get 'home', to: 'home#home', as: 'home'
-<<<<<<< HEAD
-=======
-  get 'home/home'
-  get 'home/landing'
->>>>>>> ba55d6ea7afa87d3912d0894f60b460e0caeed5b
   get 'about', to: 'about#about', as: 'about'
   get 'contact', to: 'about#contact', as: 'contact'
   get 'faq', to: 'about#faq', as: 'faq'
   get 'resources', to: 'resources#resources', as: 'resources'
-<<<<<<< HEAD
-  get 'projects', to: 'projects#index', as: 'projects'
-  get 'news', to: 'news#index', as: 'news'
-
-=======
->>>>>>> ba55d6ea7afa87d3912d0894f60b460e0caeed5b
 
   resources :challenges
   resources :resources
