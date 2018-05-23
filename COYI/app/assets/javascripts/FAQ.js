789a5ea9a6073a4523db5ab340b4f66f63dae7e6
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2018-05-23 16:28:04 +0100

    Shrunk function

--- COYI/app/assets/javascripts/FAQ.js
+++ COYI/app/assets/javascripts/FAQ.js
@@ -2,48 +2,12 @@
 // alert("You clicked")
 // })
 function myFunction(id){
-  var ans1 = document.getElementById("A1");
-  var ans2 = document.getElementById("A2");
-  var ans3 = document.getElementById("A3");
-  var ans4 = document.getElementById("A4");
-  
-  switch (id) {
-
-    case 'Q1':
-      if (ans1.style.display == "none") {
-      ans1.style.display = "block";
+  var answer = document.getElementById(id + "A");
+  // console.log(answer);
+      if (answer.style.display == "none") {
+      answer.style.display = "block";
       }
       else {
-        ans1.style.display = "none";
+        answer.style.display = "none";
       }
-      break;
-    case 'Q2':
-      if (ans2.style.display == "none") {
-      ans2.style.display = "block";
-      }
-      else {
-        ans2.style.display = "none";
-      }
-      break;
-    case 'Q3':
-      if (ans3.style.display == "none") {
-      ans3.style.display = "block";
-      }
-      else {
-        ans3.style.display = "none";
-      }
-      break;
-    case 'Q4':
-      if (ans4.style.display == "none") {
-      ans4.style.display = "block";
-      }
-      else {
-        ans4.style.display = "none";
-      }
-      break;
-    default:
-
-  }
-
-
 }
