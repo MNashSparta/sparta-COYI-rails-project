892a8bc84209d6898faa8c892698668805a464fb
Sykander <sgul@spartaglobal.com>
2018-05-23 16:11:52 +0100

    added basic profile page

--- COYI/config/routes.rb
+++ COYI/config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   root to: 'home#landing'
 
-  get 'circle/global',    to: 'circle#global', as: 'global'
-  get 'circle/chapter',   to: 'circle#chapter', as: 'chapter'
-  get 'circle/advocate',  to: 'circle#advocate', as: 'advocate'
-  get 'circle/rules',     to: 'circle#rules', as: 'rules'
-  get 'news',             to: 'news#index', as: 'news'
-  get 'home',             to: 'home#home', as: 'home'
-  get 'about',            to: 'about#about', as: 'about'
-  get 'contact',          to: 'about#contact', as: 'contact'
-  get 'faq',              to: 'about#faq', as: 'faq'
+  get 'circle/global',    to: 'circle#global',      as: 'global'
+  get 'circle/chapter',   to: 'circle#chapter',     as: 'chapter'
+  get 'circle/advocate',  to: 'circle#advocate',    as: 'advocate'
+  get 'circle/rules',     to: 'circle#rules',       as: 'rules'
+  get 'news',             to: 'news#index',         as: 'news'
+  get 'home',             to: 'home#home',          as: 'home'
+  get 'about',            to: 'about#about',        as: 'about'
+  get 'contact',          to: 'about#contact',      as: 'contact'
+  get 'faq',              to: 'about#faq',          as: 'faq'
   get 'resources',        to: 'resources#resources', as: 'resources'
-  get 'success',          to: 'success#success', as: 'stories'
-  get 'profile',          to: 'profile#profile', as: 'profile'
+  get 'success',          to: 'success#success',    as: 'stories'
+  get 'profile',          to: 'profile#profile',    as: 'profile'
 
 
   resources :challenges
