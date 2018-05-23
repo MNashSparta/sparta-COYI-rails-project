8af402116e983dfc61e4f8582708fdb9f8558717
Tech-A02 <tech-a02@Tech-A02s-MacBook-Air.local>
2018-05-23 16:36:22 +0100

    Comments have been added and function name more understandable

--- COYI/app/assets/javascripts/FAQ.js
+++ COYI/app/assets/javascripts/FAQ.js
@@ -1,7 +1,5 @@
-// document.getElementById('question1').addEventListener("click", function(){
-// alert("You clicked")
-// })
-function myFunction(id){
+// This function grabs the id of the div in the list item that was clicked and will display the div if it is hidden or hide it if it isn't.
+function displayOrHide(id){
   var answer = document.getElementById(id + "A");
   // console.log(answer);
       if (answer.style.display == "none") {
