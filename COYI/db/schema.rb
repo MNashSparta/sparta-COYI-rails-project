cbc01e67c231c2700e1b29650ea41d3b7bfa5f93
Tech-A56 <tech-a56@Tech-A56s-MacBook-Air.local>
2018-05-24 10:09:12 +0100

    commits

--- COYI/db/schema.rb
+++ COYI/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_130440) do
+ActiveRecord::Schema.define(version: 2018_05_23_133249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2018_05_23_130440) do
     t.index ["user_id"], name: "index_chapters_on_user_id"
   end
 
-
   create_table "news", force: :cascade do |t|
     t.string "title"
     t.string "story"
@@ -52,6 +51,12 @@ ActiveRecord::Schema.define(version: 2018_05_23_130440) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
