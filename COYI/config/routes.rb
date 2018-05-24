aa46b0f9cba0950a6aa103ee16655bedbb43f7f0
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-05-24 10:56:38 +0100

    change layout resource

--- COYI/config/routes.rb
+++ COYI/config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get 'faq',              to: 'about#faq',          as: 'faq'
   get 'resources',        to: 'my_resources#index', as: 'resources'
   get 'success',          to: 'success#success',    as: 'stories'
-  get 'users',          to: 'profile#profile',    as: 'user_profile'
+  get 'users',            to: 'profile#profile',    as: 'user_profile'
 
 
   resources :challenges
--- COYI/db/schema.rb
+++ COYI/db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_05_23_190319) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,10 +78,10 @@ ActiveRecord::Schema.define(version: 2018_05_23_190319) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
+    t.integer "status"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
     t.text "description"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
