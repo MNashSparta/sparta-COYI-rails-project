fc280da801e28492b3d1e515d85c3c80eea83788
HZNFSheikh <hsheikh@spartaglobal.com>
2018-05-24 14:15:14 +0100

    merging

--- COYI/db/schema.rb
+++ COYI/db/schema.rb
@@ -58,15 +58,25 @@ ActiveRecord::Schema.define(version: 2018_05_24_103938) do
     t.index ["user_id"], name: "index_chapters_on_user_id"
   end
 
+  create_table "hundred_day_challenges", force: :cascade do |t|
+    t.string "Email"
+    t.string "Address"
+    t.string "First_name"
+    t.string "Last_name"
+    t.string "Location"
+    t.string "Organisation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "my_resources", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_my_resources_on_user_id"
   end
+
   create_table "news", force: :cascade do |t|
     t.string "title"
     t.text "story"
@@ -100,13 +110,6 @@ ActiveRecord::Schema.define(version: 2018_05_24_103938) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.string "first_name"
     t.string "second_name"
     t.string "username"
@@ -118,6 +121,10 @@ ActiveRecord::Schema.define(version: 2018_05_24_103938) do
     t.datetime "updated_at", null: false
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.integer "access_level"
     t.bigint "chapter_id"
     t.index ["chapter_id"], name: "index_users_on_chapter_id"
@@ -128,7 +135,6 @@ ActiveRecord::Schema.define(version: 2018_05_24_103938) do
 
   add_foreign_key "challenges", "users"
   add_foreign_key "chapters", "users"
-  add_foreign_key "my_resources", "users"
   add_foreign_key "news", "chapters"
   add_foreign_key "news", "users"
   add_foreign_key "projects", "users"
