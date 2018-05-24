80cf533ed512252da4fece75cccd37bab37394af
Osama Ahmed <oahmed@spartaglobal.com>
2018-05-24 13:54:38 +0100

    User able to select chapter when registering

--- COYI/app/controllers/users/registrations_controller.rb
+++ COYI/app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -10,19 +10,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
-
-  # GET /resource/edit
-  def edit
-    @chapters = Chapter.all
+  def create
+    super
   end
 
-  # PUT /resource
-  # def update
-  #   super
+  # GET /resource/edit
+  # def edit
   # end
+
+  # PUT /resource
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -41,14 +40,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:chapter_id])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:chapter_id])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
