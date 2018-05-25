97d17614b7951258afc9898f0e7556e7c799b1fa
Osama Ahmed <oahmed@spartaglobal.com>
2018-05-25 12:18:49 +0100

    pull from dev

---
+++ COYI/app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  # default from: 'coyitest@gmail.com'
+  #
+  # def welcome(user)
+  #   @user = user
+  #   mail(:to => @user.email, :subject => "Welcome to My Awesome Site")
+  # end
+
+end
