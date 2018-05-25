0545292b4980015fa167350d5629efb98d84e674
Tech-A67 <mnash@spartaglobal.com>
2018-05-25 13:51:15 +0100

    fixing ask expert

--- COYI/app/models/project.rb
+++ COYI/app/models/project.rb
@@ -4,4 +4,6 @@ class Project < ApplicationRecord
   has_one :chapter, through: :user
   validates :title, :description, presence: true
   has_many_attached :uploaded_files
+  validates :title, length: { minimum: 8, too_short: "Title too short. Try and enter a descriptive title" }
+  validates :title, length: { maximum: 30, too_long: "Title too long. %{count} characters is the maximum allowed"  }
 end
